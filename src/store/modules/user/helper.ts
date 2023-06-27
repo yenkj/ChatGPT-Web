60fab79ac607aebc195a06ecac11a2b94cbbb270
yenkj <49618620+yenkj@users.noreply.github.com>
2023-06-27 16:06:00 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://raw.githubusercontent.com/yenkj/ChatGPT-Web/main/src/assets/avatar.jpg',
+      name: '香蕉的胡言乱语AI',
+      description: 'Star on <a href="https://github.com/yenkj/ChatGPT-Web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
